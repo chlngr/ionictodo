c5905381eb3d3f662826ff1c85738c3462a25444
Wolf <Wolf@Wolfs-MacBook-Air.local>
2017-03-13 17:10:15 -0400

    get the new task button working, beware of case sensitivity in camelcase!!

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -6,34 +6,53 @@
 angular.module('ionictodo', ['ionic'])
 
 // add a controller "TodoCtrl" to tell the javascript what to do with the list element
-// include some initial tasks to show
-.controller('TodoCtrl', function($scope){
+.controller('TodoCtrl', function($scope, $ionicModal) {
   $scope.tasks = [
-    { title: 'Create a button'},
-    { title: 'Have the button make an API call'},
-    { title: 'Parse the results of the API in app'},
-    { title: 'Display the parsed results in app'},
-    { title: 'Display a button that allows you to take an action'}
-
+  { title: 'test task' }
   ];
 
-
-})
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+// create but do not show a modal using the template URL new-task.html, use the animation slide-in-up to animate and load the modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
   });
+
+// upon submission of the form, push the task title to the scope, and hide the modal, reset the task title
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+};
+
+// Show the task modal on the ng-click newTask() in the center element
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+// Hide the task model on closeNewTask() called by the submit button's ng-click element
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
 })
+
+// .run(function($ionicPlatform) {
+//   $ionicPlatform.ready(function() {
+//     if(window.cordova && window.cordova.plugins.Keyboard) {
+//       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//       // for form inputs)
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+//       // Don't remove this line unless you know what you are doing. It stops the viewport
+//       // from snapping when text inputs are focused. Ionic handles this internally for
+//       // a much nicer keyboard experience.
+//       cordova.plugins.Keyboard.disableScroll(true);
+//     }
+//     if(window.StatusBar) {
+//       StatusBar.styleDefault();
+//     }
+//   });
+// })
