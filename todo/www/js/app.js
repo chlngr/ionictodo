f6b2aee4184cde580385439a43788090b71c3e32
Wolf <Wolf@Wolfs-MacBook-Air.local>
2017-03-13 17:43:01 -0400

    add a projects feature in local storage, initialize with a project if available. save all to local storage.  associate tasks with projects.

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -5,28 +5,113 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('ionictodo', ['ionic'])
 
-// add a controller "TodoCtrl" to tell the javascript what to do with the list element
-.controller('TodoCtrl', function($scope, $ionicModal) {
-  $scope.tasks = [
-  { title: 'test task' }
-  ];
+//define your factories for saving and loading projects from local storage, and save and load last active projects.
+//define all, save, new, getactive, setlastactive functions for the factory
 
-// create but do not show a modal using the template URL new-task.html, use the animation slide-in-up to animate and load the modal
+.factory('Projects', function() {
+  return {
+    all: function() {
+      var projectString = window.localStorage['projects'];
+      if(projectString) {
+        return angular.fromJson(projectString);
+      }
+      return [];
+    },
+    save: function(projects) {
+      window.localStorage['projects'] = angular.toJson(projects);
+    },
+    newProject: function(projectTitle) {
+      //add a new project
+      return {
+        title: projectTitle,
+        tasks: []
+      };
+    },
+    getLastActiveIndex: function() {
+      return parseInt(window.localStorage['lastActiveProject']) || 0;
+    },
+    setLastActiveIndex: function(index) {
+      window.localStorage['lastActiveProject'] = index;
+    }
+  }
+})
+
+
+
+
+
+// add a controller "TodoCtrl" to tell the javascript what to do with the list element
+// add timeout and ionicsidemenu references, and refer to projects in local storage
+.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
+  // create a new project with a given project title, push to scope
+  // save to scope
+  // select the last appended project (relies on order)
+  var createProject = function(projectTitle) {
+    var newProject = Projects.newProject(projectTitle);
+    $scope.projects.push(newProject);
+    Projects.save($scope.projects);
+    $scope.selectProject(newProject, $scope.projects.length-1);
+  }
+
+  // load projects
+  $scope.projects = Projects.all();
+
+  // grab the last active or only project
+  $scope.activeProject = $scope.projects[Projects.getLastActiveIndex];
+
+  // create a new project
+  $scope.newProject = function() {
+    var projectTitle = prompt('Project name');
+    if(projectTitle) {
+      createProject(projectTitle);
+    }
+  };
+
+  $scope.selectProject = function(project, index) {
+    $scope.activeProject = project;
+    Projects.setLastActiveIndex(index);
+    $ionicSideMenuDelegate.toggleLeft(false);
+  };
+
+  //create the modal
   $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
     $scope.taskModal = modal;
   }, {
-    scope: $scope,
-    animation: 'slide-in-up'
+    scope: $scope
   });
 
-// upon submission of the form, push the task title to the scope, and hide the modal, reset the task title
   $scope.createTask = function(task) {
-    $scope.tasks.push({
+    if(!$scope.activeProject || !task) {
+      return;
+    }
+    $scope.activeProject.tasks.push({
       title: task.title
     });
     $scope.taskModal.hide();
+
+    Projects.save($scope.project);
     task.title = "";
-};
+  };
+
+
+  $scope.tasks = [];
+
+// // create but do not show a modal using the template URL new-task.html, use the animation slide-in-up to animate and load the modal
+//   $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+//     $scope.taskModal = modal;
+//   }, {
+//     scope: $scope,
+//     animation: 'slide-in-up'
+//   });
+
+// // upon submission of the form, push the task title to the scope, and hide the modal, reset the task title
+//   $scope.createTask = function(task) {
+//     $scope.tasks.push({
+//       title: task.title
+//     });
+//     $scope.taskModal.hide();
+//     task.title = "";
+// };
 
 // Show the task modal on the ng-click newTask() in the center element
   $scope.newTask = function() {
@@ -37,7 +122,28 @@ angular.module('ionictodo', ['ionic'])
   $scope.closeNewTask = function() {
     $scope.taskModal.hide();
   };
+
+  $scope.toggleProjects = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+  // Try to create the first project, make sure to defer
+  // this by using $timeout so everything is initialized
+  // properly
+  $timeout(function() {
+    if($scope.projects.length == 0) {
+      while(true) {
+        var projectTitle = prompt('Your first project title:');
+        if(projectTitle) {
+          createProject(projectTitle);
+          break;
+        }
+      }
+    }
+  }, 1000);
+
 })
+
 
 // .run(function($ionicPlatform) {
 //   $ionicPlatform.ready(function() {
